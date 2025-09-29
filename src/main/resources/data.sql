665498ecaee762a0e3ef0c38f94215f71d40bd71
idowu moses <dynamopraize5050@gmail.com>
2025-09-30 00:55:38 +0100

    Remove data.sql from repo

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO box (id, txref, weight_limit, battery_capacity, state) VALUES
-            (1, 'BOX_001', 500, 90, 'IDLE'),
-            (2, 'BOX_002', 500, 30, 'IDLE'),
-            (3, 'BOX_003', 500, 20, 'IDLE'),
-            (4, 'BOX_004', 500, 80, 'IDLE'),
-            (5, 'BOX_005', 500, 50, 'IDLE');
