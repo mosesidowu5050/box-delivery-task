4558f391139f34dd4502da5fa6ec62838eb079c2
idowu moses <dynamopraize5050@gmail.com>
2025-09-30 06:59:43 +0100

    update code

--- src/main/java/org/mosesidowu/boxdelivery/controller/BoxController.java
+++ src/main/java/org/mosesidowu/boxdelivery/controller/BoxController.java
@@ -1,9 +1,9 @@
 package org.mosesidowu.boxdelivery.controller;
 
-
 import lombok.RequiredArgsConstructor;
 import org.mosesidowu.boxdelivery.dtos.request.BoxRequest;
 import org.mosesidowu.boxdelivery.dtos.request.ItemRequest;
+import org.mosesidowu.boxdelivery.dtos.response.ApiResponse;
 import org.mosesidowu.boxdelivery.dtos.response.BoxResponse;
 import org.mosesidowu.boxdelivery.dtos.response.ItemResponse;
 import org.mosesidowu.boxdelivery.service.BoxService;
@@ -16,31 +16,32 @@ import java.util.Map;
 @RequestMapping("/api/boxes")
 @RequiredArgsConstructor
 public class BoxController {
+
     private final BoxService boxService;
 
-    @PostMapping
-    public ResponseEntity<BoxResponse> createBox(@RequestBody BoxRequest req) {
-        return ResponseEntity.ok(boxService.createBox(req));
+    @PostMapping("/create-box")
+    public ApiResponse<BoxResponse> createBox(@RequestBody BoxRequest req) {
+        return ApiResponse.success(boxService.createBox(req));
     }
 
-    @PostMapping("/{id}/load")
-    public ResponseEntity<BoxResponse> loadBoxWithItems(@PathVariable Long id, @RequestBody List<ItemRequest> requests) {
-        return ResponseEntity.ok(boxService.loadBoxWithItems(id, requests));
+    @PostMapping("/{id}/load-items")
+    public ApiResponse<BoxResponse> loadBoxWithItems(@PathVariable Long id, @RequestBody List<ItemRequest> requests) {
+        return ApiResponse.success(boxService.loadBoxWithItems(id, requests));
     }
 
-    @GetMapping("/{id}/items")
-    public ResponseEntity<List<ItemResponse>> getLoadedItems(@PathVariable Long id) {
-        return ResponseEntity.ok(boxService.checkLoadedItems(id));
+    @GetMapping("/{id}/get-items")
+    public ApiResponse<List<ItemResponse>> getLoadedItems(@PathVariable Long id) {
+        return ApiResponse.success(boxService.checkLoadedItems(id));
     }
 
-    @GetMapping("/available")
-    public ResponseEntity<List<BoxResponse>> getAvailableBoxesForLoading() {
-        return ResponseEntity.ok(boxService.checkAvailableBoxesForLoading());
+    @GetMapping("/get_available-items")
+    public ApiResponse<List<BoxResponse>> getAvailableBoxesForLoading() {
+        return ApiResponse.success(boxService.checkAvailableBoxesForLoading());
     }
 
-    @GetMapping("/{id}/battery")
-    public ResponseEntity<?> getBatteryLevel(@PathVariable Long id) {
+    @GetMapping("/{id}/battery-level")
+    public ApiResponse<?> getBatteryLevel(@PathVariable Long id) {
         int batteryLevel = boxService.checkBatteryLevel(id);
-        return ResponseEntity.ok().body(Map.of("batteryLevel", batteryLevel));
+        return ApiResponse.success(Map.of("batteryLevel", batteryLevel));
     }
 }
